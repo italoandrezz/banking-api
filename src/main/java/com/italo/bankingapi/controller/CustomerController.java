331d1fe9e9454df92509ab54bc6e0e4e48af545e
Italo <italo4dev@gmail.com>
2026-07-20 15:18:04 -0300

    feat(customer): implement customer search by id

--- src/main/java/com/italo/bankingapi/controller/CustomerController.java
+++ src/main/java/com/italo/bankingapi/controller/CustomerController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/customers")
@@ -25,5 +24,10 @@ public class CustomerController {
         return  ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findCustomerById(@PathVariable UUID id) {
+        CustomerResponse response = customerService.findCustomerById(id);
+        return ResponseEntity.ok(response);
     }
 }
